a8ef3cd10839ac3cc5fa7fe55b748852ed94e191
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-10-01 19:21:19 +0100

    Added images

---
+++ script.js
@@ -0,0 +1,100 @@
+const cardArray = [
+    {
+        name: '1',
+        img: 'images/1.jpg',
+    },
+    {
+        name: '2',
+        img: 'images/2.jpg',
+    },
+    {
+        name: '3',
+        img: 'images/3.jpg',
+    },
+    {
+        name: '4',
+        img: 'images/4.jpg',
+    },
+    {
+        name: '5',
+        img: 'images/5.jpg',
+    },
+    {
+        name: '6',
+        img: 'images/6.jpg',
+    },
+    {
+        name: '7',
+        img: 'images/7.jpg',
+    },
+    {
+        name: '8',
+        img: 'images/8.jpg',
+    },
+    {
+        name: '9',
+        img: 'images/9.jpg',
+    },
+    {
+        name: '10',
+        img: 'images/10.jpg',
+    },
+    {
+        name: '11',
+        img: 'images/11.jpg',
+    },
+    {
+        name: '12',
+        img: 'images/12.jpg',
+    },
+    {
+        name: '1',
+        img: 'images/1.jpg',
+    },
+    {
+        name: '2',
+        img: 'images/2.jpg',
+    },
+    {
+        name: '3',
+        img: 'images/3.jpg',
+    },
+    {
+        name: '4',
+        img: 'images/4.jpg',
+    },
+    {
+        name: '5',
+        img: 'images/5.jpg',
+    },
+    {
+        name: '6',
+        img: 'images/6.jpg',
+    },
+    {
+        name: '7',
+        img: 'images/7.jpg',
+    },
+    {
+        name: '8',
+        img: 'images/8.jpg',
+    },
+    {
+        name: '9',
+        img: 'images/9.jpg',
+    },
+    {
+        name: '10',
+        img: 'images/10.jpg',
+    },
+    {
+        name: '11',
+        img: 'images/11.jpg',
+    },
+    {
+        name: '12',
+        img: 'images/12.jpg',
+    },
+]
+
+console.log(cardArray)
